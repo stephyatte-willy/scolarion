8ab3929fa46e22b6bafb99628350782b2ab9fe2f
stephyatte-willy <stephyatte@gmail.com>
2026-03-04 00:23:43 +0000

    Update database.ts

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',  // ou localhost
+  host: process.env.DB_HOST || '91.216.107.196',  // ou localhost
   user: process.env.DB_USER || 'westc2710564_1wzu2u', 
   password: process.env.DB_PASSWORD || '6fxm3jycls',
   database: process.env.DB_NAME || 'westc2710564_1wzu2u',
