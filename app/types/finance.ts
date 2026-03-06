04fd46be8a93e11d8ab35bf7ca1828300bbd0afb
stephyatte-willy <stephyatte@gmail.com>
2026-03-06 18:10:57 +0000

    Modifier finance dans type

--- app/types/finance.ts
+++ app/types/finance.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // app/types/finance.ts
 export interface Paiement {
   id: number;
@@ -23,30 +23,4 @@ export interface Paiement {
   annee_scolaire?: string;
   statut?: string;
   [key: string]: any;
-=======
-// app/types/finance.ts
-export interface Paiement {
-  id: number;
-  frais_eleve_id: number;
-  eleve_id: number;
-  eleve_nom?: string;
-  eleve_prenom?: string;
-  eleve_matricule?: string;
-  classe_nom?: string;
-  classe_niveau?: string;
-  categorie_nom?: string;
-  montant: number;
-  montant_paye?: number;
-  montant_total?: number;
-  reste_a_payer?: number;
-  mode_paiement: string;
-  date_paiement: string;
-  reference_paiement?: string;
-  numero_recu?: string;
-  numero_versement?: number;
-  created_by?: number;
-  annee_scolaire?: string;
-  statut?: string;
-  [key: string]: any;
->>>>>>> 32d4e54ab2267c749063a541db8ca8150d07c1c5
 }
